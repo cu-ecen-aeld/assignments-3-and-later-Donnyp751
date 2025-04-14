282acc18fc345be38b461a8357ddefa5f3226484
Donald Posterick <donny.p@live.com>
2025-04-14 04:07:11 +0000

    Added linux image build, rootfs setup, finder app cross-compile, and rootfs cpio creation

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,7 +11,11 @@ KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE_BASE=aarch64-none-linux-gnu
+CROSS_COMPILE=${CROSS_COMPILE_BASE}-
+ROOTFS_DIRECTORIES=("/bin" "/lib" "/lib64" "/usr" "/var" "/etc" "/home" "/home/conf" "/root" "/dev" "/tmp" "/proc" "/sys" "/usr/bin" "usr/lib" "/usr/sbin" "/var/log")
+ROOTFS=${OUTDIR}/rootfs
+TOOLCHAIN_ROOTFS=$(${CROSS_COMPILE}gcc --print-sysroot)
 
 if [ $# -lt 1 ]
 then
@@ -33,11 +37,12 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
-    # TODO: Add your kernel build steps here
+    cp ${FINDER_APP_DIR}/../conf/.k_config ${OUTDIR}/linux-stable/.config
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +52,15 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+
+for dir in "${ROOTFS_DIRECTORIES[@]}"; do
+    target_dir=${ROOTFS}/$dir
+    if [ ! -d $target_dir ]; then
+        mkdir -p $target_dir
+    else
+        echo "$target_dir already exists"
+    fi
+done
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +68,53 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+     cp ${FINDER_APP_DIR}/../conf/.bb_config ${OUTDIR}/busybox/.config
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+echo "Building Busybox"
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install CONFIG_PREFIX=${ROOTFS}
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+interpreter=$(${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "program interpreter" |  grep -oP "/?[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*\.so\.[0-9]+")
+libraries=$(${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "Shared library" |  grep -oP "/?[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*\.so\.[0-9]+")
+toolchain_libraries=${TOOLCHAIN_ROOTFS}/lib64
 
-# TODO: Add library dependencies to rootfs
 
-# TODO: Make device nodes
+echo "Copying interpreter $interpreter from ${TOOLCHAIN_ROOTFS} to ${ROOTFS}"
+cp ${TOOLCHAIN_ROOTFS}/$interpreter ${ROOTFS}/lib
+
+# Copy the libraries
+echo "Copying libraries $libraries from $toolchain_libraries to ${ROOTFS}"
+for l in $libraries; do
+    cp $toolchain_libraries/$l ${ROOTFS}/lib64
+done
+
+# Make device nodes
+echo "Creating device nodes"
+#cant do this on a mounted device
+#cd ${ROOTFS}
+mknod -m 666 dev/null c 1 3
+mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
 
+make -C ${FINDER_APP_DIR} clean
+make -C ${FINDER_APP_DIR} CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH}
+
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/writer ${ROOTFS}/home
+cp ${FINDER_APP_DIR}/writer.sh ${ROOTFS}/home
+cp ${FINDER_APP_DIR}/finder.sh ${ROOTFS}/home
+cp ${FINDER_APP_DIR}/finder-test.sh ${ROOTFS}/home
+
+cp ${FINDER_APP_DIR}/conf/*.txt ${ROOTFS}/home/conf
 
 # TODO: Chown the root directory
 
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
